441e5c181ce7190c8c4fdf98c6157943e4d55d37
seyma.cakmak <seyma.cakmak@akgun.com.tr>
2024-03-29 00:26:44 +0300

    Make an api call to ChatGPT

--- src/main/java/com/musicapp/config/ApplicationProperties.java
+++ src/main/java/com/musicapp/config/ApplicationProperties.java
@@ -3,7 +3,7 @@ package com.musicapp.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
- * Properties specific to Bitirme Projesi.
+ * Properties specific to Music App.
  * <p>
  * Properties are configured in the {@code application.yml} file.
  * See {@link tech.jhipster.config.JHipsterProperties} for a good example.
